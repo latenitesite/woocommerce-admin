4a5ee8fe9fa6b6966f68a4192be2429a21b0fa6f
Lourens Schep <lourensschep@gmail.com>
2021-05-04 16:38:36 -0300

    Update tracks

--- client/task-list/task-list.js
+++ client/task-list/task-list.js
@@ -178,7 +178,7 @@ export const TaskList = ( {
 		);
 		const isCoreTaskList = name === 'task_list';
 		const taskListName = isCoreTaskList ? 'tasklist' : 'extended_tasklist';
-		recordEvent( `${ taskListName }_task_remindmelater`, {
+		recordEvent( `${ taskListName }_remindmelater_task`, {
 			task_name: key,
 		} );
 
@@ -277,6 +277,13 @@ export const TaskList = ( {
 	const listTasks = getVisibleTasks().map( ( task ) => {
 		if ( ! task.onClick ) {
 			task.onClick = ( e ) => {
+				const isCoreTaskList = name === 'task_list';
+				const taskListName = isCoreTaskList
+					? 'tasklist'
+					: 'extended_tasklist';
+				recordEvent( `${ taskListName }_click`, {
+					task_name: task.key,
+				} );
 				if ( e.target.nodeName === 'A' ) {
 					// This is a nested link, so don't activate this task.
 					return false;
